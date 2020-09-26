2923e0253de58855216fa2f9363783c036e41ad4
kizumteam <wild.guevara@ucalgary.ca>
2020-09-25 18:34:32 -0600

    August 28th Version
    All files here were created and put on the website on August 28th. Includes method for handling cookies.

---
+++ contact-sender.php
@@ -0,0 +1,15 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$email_to = "ayts.ca@gmail.com";
+	$email_subject = "CONTACT Form Submission From $name";
+	$email_body = "Received a new CONTACT FORM submission from EMAIL:\n $email\n\n".
+					"Their NAME is:\n $name\n\n".
+					"Their MESSAGE is:\n $message\n";
+
+	mail($email_to, $email_subject, $email_body);
+	
+	header("Location: success.html");
+?>
