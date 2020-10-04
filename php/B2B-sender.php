633909caae09ab25f41fa8c2b865448c90bff5a2
kizumteam <wild.guevara@ucalgary.ca>
2020-10-03 19:23:59 -0600

    Contact Form Update
    Now the sender will get an email copy of what they sent.

--- php/B2B-sender.php
+++ php/B2B-sender.php
@@ -9,11 +9,18 @@
 	$email_subject = "B2B Form Submission From $name";
 	$email_body = "Received a new B2B FORM submission from EMAIL:\n $email\n\n".
 					"Their NAME is:\n $name\n\n".
-					"Their COMPANY is: \n $company\n\n";
+					"Their COMPANY is: \n $company\n\n".
 					"Their MESSAGE is:\n $message\n";
 
-	mail($email_to, $email_subject, $email_body);
-	mail($email_to_backup, $email_subject, $email_body);
+    mail($email_to, $email_subject, $email_body);
+    mail($email_to_backup, $email_subject, $email_body);
+
+    $header_sender = "From: b2b@ayts.ca";
+    $email_subject_sender = "Thank You For Getting In Contact $name";
+	$email_body_sender = "Thank you for getting in contact with Ayts. You wrote the following message:\n $message\n\n".
+	                    "We will get back to you shortly.";
+
+	mail($email, $email_subject_sender, $email_body_sender, $header_sender);
 
 	header("Location: ../success.html");
 ?>
--- php/invest-sender.php
+++ php/invest-sender.php
@@ -13,5 +13,12 @@
 	mail($email_to, $email_subject, $email_body);
 	mail($email_to_backup, $email_subject, $email_body);
 
+    $header_sender = "From: contact@ayts.ca";
+    $email_subject_sender = "Thank You For Getting In Contact $name";
+    $email_body_sender = "Thank you for getting in contact with Ayts. You wrote the following message:\n $message\n\n".
+                        "We will get back to you shortly.";
+
+    mail($email, $email_subject_sender, $email_body_sender, $header_sender);
+
 	header("Location: ../success.html");
 ?>
